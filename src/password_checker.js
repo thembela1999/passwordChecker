518b1d78b0b61462fce2e1cceda9e590ba2f3a8d
thembela1999 <mbelaselani@gmail.com>
2020-05-26 12:40:35 +0200

    updated

--- src/password_checker.js
+++ src/password_checker.js
@@ -1,45 +1,47 @@
 function passwordIsValid(password) {
-  try { 
-   if (password == "") throw "password should exist";
-   if (password.length < 8)
-     throw "password should be longer than than 8 characters";
-   if (password.match(/[a-z]/g) == null) 
-     throw "password should have at least one lowercase letter";
-   if (password.match(/[A-Z]/g) == null)
-     throw "password should have at least one uppercase letter";
-   if (password.match(/[$&+,:;=?@#|'<>.^*()%!-]/g) == null)
-     throw $("password should have at least one special character");
-   if (password.match(/[0-9]/g) == null)
-     throw "password should at least have one digit";
-  
+    try {
+        if (password == "") throw "password should exist";
+        if (password.length <= 8)
+            throw "password should be longer than than 8 characters";
+        if (password.match(/[a-z]/g) == null)
+            throw "password should have at least one lowercase letter";
+        if (password.match(/[A-Z]/g) == null)
+            throw "password should have at least one uppercase letter";
+        if (password.match(/[$&+,:;=?@#|'<>.^*()%!-]/g) == null)
 
-   return `password is valid`;
- } catch (error) { 
-   console.error(`Invalid password: ${error}`);
- }
- return passwordIsValid;
+            throw $("password should have at least one special character");
+        if (password.match(/[0-9]/g) == null)
+
+            throw "password should at least have one digit";
+
+
+        return `password is valid`;
+    } catch (error) {
+        console.error(`Invalid password: ${error}`);
+    }
+    return passwordIsValid;
 }
 
 console.log(passwordIsValid("Bell@210"));
 
 function passwordIsOk(password) {
-  const condition = [/[0-9]/, /[A-Z]/, /[a-z]/, /"!@#$%*()."/];
-  if (password == "" || password.length < 8) {
-    return false;
-  }
-
-  for (let i = 0; i < condition.length; i++) {
-    if (password.match(condition[i])) {
-      return true;
-    } else {
-      return false;
+    const condition = [/[0-9]/, /[A-Z]/, /[a-z]/, /"!@#$%*()."/];
+    if (password == "" || password.length < 8) {
+        return false;
     }
-  }
+
+    for (let i = 0; i < condition.length; i++) {
+        if (password.match(condition[i])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 console.log(passwordIsOk("Bel1g210@"));
 
-module.exports = { 
+module.exports = {
 
- passwordIsValid,
-passwordIsOk
+    passwordIsValid,
+    passwordIsOk
 };
